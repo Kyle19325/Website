97a78c5d3d7c83ef96cf19ce5e491f94d2c73b56
Kyle19325 <kyle19325@gmail.com>
2017-06-12 09:01:42 -0500

    Create main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function main(){
-  var $test = $(".test");
-  $test.hide();
-  $test.fadeIn(2000);
+  var $header = $(".header");
+  $header.hide();
+  $header.fadeIn(2000);
 }
 $(document).ready(main);
